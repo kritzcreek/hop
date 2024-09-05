a5a81cd7702052b76afcb5d09482e023ed642ecf
Christoph <christoph.hegemann@sourcegraph.com>
2024-09-05 08:40:20 +0200

    adds a list-files command

--- crates/hop/src/main.rs
+++ crates/hop/src/main.rs
@@ -2,7 +2,11 @@ mod proto;
 
 use anyhow::{bail, Context, Result};
 use clap::{Parser, Subcommand};
-use std::{fs, io::Read, path::PathBuf};
+use std::{
+    fs,
+    io::Read,
+    path::{Path, PathBuf},
+};
 
 /// The hop toolchain
 #[derive(Debug, Parser)]
@@ -22,21 +26,30 @@ enum Commands {
         #[arg(long)]
         path: Option<String>,
     },
+
+    /// Lists the paths of documents contained within the given index as json
+    ListFiles { input: Option<PathBuf> },
+}
+
+fn read_input(input: Option<&Path>) -> Result<Vec<u8>> {
+    match input {
+        Some(file) => {
+            fs::read(file).context(format!("Failed to read input file at {}", file.display()))
+        }
+        None => {
+            let mut buffer: Vec<u8> = Vec::new();
+            std::io::stdin().read_to_end(&mut buffer)?;
+            Ok(buffer)
+        }
+    }
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
     match args.command {
         Commands::Print { input, path } => {
-            let input = match input {
-                Some(file) => fs::read(file)?,
-                None => {
-                    let mut buffer: Vec<u8> = Vec::new();
-                    std::io::stdin().read_to_end(&mut buffer)?;
-                    buffer
-                }
-            };
-            if let Some(index) = proto::read_index(&input) {
+            let bytes = read_input(input.as_deref())?;
+            if let Some(index) = proto::read_index(&bytes) {
                 if let Some(path) = path {
                     let document = index
                         .documents
@@ -47,13 +60,26 @@ fn main() -> Result<()> {
                 } else {
                     println!("{}", serde_json::to_string(&index)?);
                 }
-            } else if let Some(document) = proto::read_document(&input) {
+            } else if let Some(document) = proto::read_document(&bytes) {
                 if let Some(_) = path {
                     bail!("Cannot filter by --path when searching a single encoded document");
                 }
                 println!("{}", serde_json::to_string(&document)?);
             } else {
                 bail!("Could not parse input as either an index or a document");
+            }
+            Ok(())
+        }
+        Commands::ListFiles { input } => {
+            let bytes = read_input(input.as_deref())?;
+            if let Some(index) = proto::read_index(&bytes) {
+                let mut paths: Vec<_> = index.documents.iter().map(|d| &d.relative_path).collect();
+                paths.sort();
+                println!("{}", serde_json::to_string(&paths)?);
+            } else if let Some(document) = proto::read_document(&bytes) {
+                println!("{}", serde_json::to_string(&vec![document.relative_path])?);
+            } else {
+                bail!("Could not parse input as either an index or a document")
             }
             Ok(())
         }
