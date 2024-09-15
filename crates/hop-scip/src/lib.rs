beff79ba1c71cbdcefc65ee23b4f6c5a3d14bf59
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2024-09-15 10:07:46 +0200

    reorg

--- crates/hop-scip/src/lib.rs
+++ crates/hop-scip/src/lib.rs
@@ -1,6 +1,18 @@
-pub mod symbol;
-
 #[allow(clippy::all)]
 mod proto;
+pub mod symbol;
+pub use proto::scip;
 
-pub use proto::{encode_index, read_document, read_index, scip};
+use prost::Message;
+
+pub fn read_document(input: &[u8]) -> Option<scip::Document> {
+    scip::Document::decode(input).ok()
+}
+
+pub fn read_index(input: &[u8]) -> Option<scip::Index> {
+    scip::Index::decode(input).ok()
+}
+
+pub fn encode_index(index: scip::Index) -> Vec<u8> {
+    index.encode_to_vec()
+}
--- crates/hop-scip/src/proto.rs
+++ crates/hop-scip/src/proto.rs
@@ -1,15 +1,1 @@
-use prost::Message;
-
 pub mod scip;
-
-pub fn read_document(input: &[u8]) -> Option<scip::Document> {
-    scip::Document::decode(input).ok()
-}
-
-pub fn read_index(input: &[u8]) -> Option<scip::Index> {
-    scip::Index::decode(input).ok()
-}
-
-pub fn encode_index(index: scip::Index) -> Vec<u8> {
-    index.encode_to_vec()
-}
